6dc695486e307dcf15c9ea61d1633b4f82cd8e83
Lukas Petry <lukemt@rennwurst.de>
2021-09-13 15:51:18 +0200

    Simplify App Component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,47 +1,10 @@
-import logo from "./logo.svg";
+import Header from "../header/Header";
 import "./App.css";
 
 function App() {
-  const someTextDescrition = "Here is someText:";
-  let someText = "Ho ho ho";
-  const url = "https://reactjs.org";
-  const showLink = true;
-
-  // const linkHint = showLink ? "with link!" : "";
-  const linkHint = showLink && "with link!";
-
   return (
     <div className="App">
-      <header className="App-header">
-        {linkHint}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {someTextDescrition} {someText}
-        </p>
-        {showLink ? (
-          <a
-            className="App-link"
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Amazing React!
-          </a>
-        ) : (
-          ""
-        )}
-
-        {showLink && (
-          <a
-            className="App-link"
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Amazing React!
-          </a>
-        )}
-      </header>
+      <Header />
     </div>
   );
 }
