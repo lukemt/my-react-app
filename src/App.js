f3315ca0207a0c9e6e37127e5c860b51cb069ff3
Lukas Petry <lukemt@rennwurst.de>
2021-09-13 12:55:27 +0200

    add example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const someTextDescrition = "Here is someText:";
+  let someText = "Ho ho ho";
+  const url = "https://reactjs.org";
+  const showLink = true;
+
+  // const linkHint = showLink ? "with link!" : "";
+  const linkHint = showLink && "with link!";
+
   return (
     <div className="App">
       <header className="App-header">
+        {linkHint}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {someTextDescrition} {someText}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {showLink ? (
+          <a
+            className="App-link"
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Amazing React!
+          </a>
+        ) : (
+          ""
+        )}
+
+        {showLink && (
+          <a
+            className="App-link"
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Amazing React!
+          </a>
+        )}
       </header>
     </div>
   );
