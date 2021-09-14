2ae6e04e66d563f1c14eed05ca3c505494afe845
Lukas Petry <lukemt@rennwurst.de>
2021-09-14 15:27:59 +0200

    add js data iteration

--- src/02-tuesday/components/App.js
+++ src/02-tuesday/components/App.js
@@ -1,17 +1,43 @@
 import "./App.css";
 import Header from "./Header";
 import Card from "./Card";
-import data from "../../data.json";
+import data from "../data.json";
 import Footer from "./Footer";
 
+/*
+[
+  {
+    "name": "Harry Potter",
+    "species": "human",
+    "gender": "male",
+    "house": "Gryffindor",
+    "dateOfBirth": "31-07-1980",
+    "yearOfBirth": 1980,
+    "ancestry": "half-blood",
+    "eyeColour": "green",
+    "hairColour": "black",
+    "wand": { "wood": "holly", "core": "phoenix feather", "length": 11 },
+    "patronus": "stag",
+    "hogwartsStudent": true,
+    "hogwartsStaff": false,
+    "actor": "Daniel Radcliffe",
+    "alive": true,
+    "image": "http://hp-api.herokuapp.com/images/harry.jpg"
+  },
+]
+*/
 function App() {
-  console.log(data);
   return (
     <div className="App">
       <Header title="Harry Potter App" />
-      <Card characterName="Hermione Granger" house="Gryffindor" imgUrl="" />
-      <Card characterName="Harry Potter" house="Gryffindor" imgUrl="" />
-      <Card characterName="Ron Weasleyyyy" house="Gryffindor" imgUrl="" />
+      {data.map((character) => (
+        <Card
+          characterName={character.name}
+          house={character.house}
+          imgUrl={character.image}
+          key={character.name}
+        />
+      ))}
       <Footer />
     </div>
   );
