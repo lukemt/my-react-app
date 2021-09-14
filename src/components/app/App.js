2e9d23d7cbcb1d854ed4c1e2e3693cf6d75869b9
Lukas Petry <lukemt@rennwurst.de>
2021-09-14 11:07:34 +0200

    ternary and if and className example

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
+import "./App.module.css";
 import Header from "../header/Header";
-import "./App.css";
+import CardHermione from "../CardHermione/CardHermione";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CardHermione />
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,11 @@
-import "./Header.css";
+import styles from "./Header.module.css";
 
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className={styles.App}>Header</div>
+    </header>
+  );
 }
 
 export default Header;
