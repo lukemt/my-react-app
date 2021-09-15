a8dff53b30a4914fae4f8f77b92f4b4743313940
Lukas Petry <lukemt@rennwurst.de>
2021-09-15 17:46:31 +0200

    implement basic footer component

--- src/03-wednesday/components/Footer.js
+++ src/03-wednesday/components/Footer.js
@@ -1,4 +1,37 @@
+import { useState } from "react";
+import "./Footer.css";
+
 function Footer() {
-  return <footer></footer>;
+  const [activeHouse, setActiveHouse] = useState("");
+
+  return (
+    <footer>
+      <h2>{activeHouse}</h2>
+      <button
+        className={activeHouse === "Gryffindor" ? "footer__button--active" : ""}
+        onClick={() => setActiveHouse("Gryffindor")}
+      >
+        G
+      </button>
+      <button
+        className={activeHouse === "Hufflepuff" ? "footer__button--active" : ""}
+        onClick={() => setActiveHouse("Hufflepuff")}
+      >
+        H
+      </button>
+      <button
+        className={activeHouse === "Ravenclaw" ? "footer__button--active" : ""}
+        onClick={() => setActiveHouse("Ravenclaw")}
+      >
+        R
+      </button>
+      <button
+        className={activeHouse === "Slytherin" ? "footer__button--active" : ""}
+        onClick={() => setActiveHouse("Slytherin")}
+      >
+        S
+      </button>
+    </footer>
+  );
 }
 export default Footer;
