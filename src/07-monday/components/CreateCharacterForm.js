20e2aff9e88e9286d60330f9505e46b7398c249d
Lukas Petry <lukemt@rennwurst.de>
2021-09-20 16:11:36 +0200

    call onCreateCharacter in CreateCharacterForm

--- src/07-monday/components/CreateCharacterForm.js
+++ src/07-monday/components/CreateCharacterForm.js
@@ -1,15 +1,20 @@
 import "./CreateCharacterForm.css";
 
-function CreateCharacterForm() {
+function CreateCharacterForm({ onCreateCharacter }) {
   return (
     <form onSubmit={(event) => handleSubmit(event)}>
       <label>
         Name of Character:
-        <input type="text" name="characterName"></input>
+        <input
+          type="text"
+          name="characterName"
+          required
+          autoComplete="Off"
+        ></input>
       </label>
       <label>
         House:
-        <input type="text" name="house"></input>
+        <input type="text" name="house" required></input>
       </label>
       <button>create</button>
     </form>
@@ -33,6 +38,17 @@ function CreateCharacterForm() {
     console.log("form.elements", form.elements);
     console.log("name of character: " + characterName.value);
     console.log("haus: " + house.value);
+
+    // exkurs return
+    // if (characterName.value === "") {
+    //   return;
+    // }
+
+    onCreateCharacter({
+      name: characterName.value,
+      house: house.value,
+    });
+
     form.reset();
     characterName.focus();
   }
