2ad165469800f7b89621fbe58549bc5e09a8abd5
Lukas Petry <lukemt@rennwurst.de>
2021-09-22 12:55:37 +0200

    add comments

--- src/09-wednesday/charactersApp/PersonRoute.js
+++ src/09-wednesday/charactersApp/PersonRoute.js
@@ -2,8 +2,10 @@ import { useParams } from "react-router-dom";
 import Card from "./Card";
 
 export default function PersonRoute({ data }) {
+  // get name from the url
   const { name } = useParams();
 
+  // search data array for the data of the person
   const personInfo = data.find(
     (person) => person.name.toLowerCase() === name.toLowerCase()
   );
