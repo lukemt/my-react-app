b44223bf3c585953c08006f1cfb329bfa6d1df98
Lukas Petry <lukemt@rennwurst.de>
2021-09-26 17:02:25 +0200

    change header to styled component

--- src/11-friday/harryPotter/Header.js
+++ src/11-friday/harryPotter/Header.js
@@ -1,12 +1,30 @@
-import "./Header.css";
+import styled from "styled-components";
 import logo from "./HeaderLogo.svg";
 
 function Header() {
   return (
-    <header className="header">
-      <img src={logo} />
-    </header>
+    <StyledHeader className="header">
+      <StyledImg src={logo} alt="" />
+    </StyledHeader>
   );
 }
+
+const StyledHeader = styled.header`
+  padding: 30px;
+  text-align: center;
+
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0px 0px 20px #dedede;
+  position: sticky;
+  width: 100%;
+  top: 0;
+  left: 0;
+`;
+
+const StyledImg = styled.img`
+  display: block;
+  height: 70px;
+  margin: 0 auto;
+`;
 
 export default Header;
